6cef59eb0bc61a1afc0075d27b362737bade7ac1
novy <45167857+novysodope@users.noreply.github.com>
2024-06-14 11:55:05 +0800

    Update CodeAuditMain.java

--- CodeAuditMain.java
+++ CodeAuditMain.java
@@ -31,16 +31,62 @@ import java.util.*;
  **/
 public class CodeAuditMain {
 
+    //优化输出，显示完整的调用链
+    static class VulnerabilityDetail {
+        String xmlFile;
+        String methodName;
+        int xmlLineNumber;
+        List<String> implCalls;
+        List<String> controllerCalls;
+
+        VulnerabilityDetail(String xmlFile, String methodName, int xmlLineNumber) {
+            this.xmlFile = xmlFile;
+            this.methodName = methodName;
+            this.xmlLineNumber = xmlLineNumber;
+            this.implCalls = new ArrayList<>();
+            this.controllerCalls = new ArrayList<>();
+        }
+
+        void addImplCall(String call) {
+            this.implCalls.add(call);
+        }
+
+        void addControllerCall(String call) {
+            this.controllerCalls.add(call);
+        }
+
+        List<String> getFormattedOutput() {
+            List<String> outputs = new ArrayList<>();
+            String base = String.format("%s 的 %s 方法存在注入，在第 %d 行", xmlFile, methodName, xmlLineNumber);
+
+            if (implCalls.isEmpty() && controllerCalls.isEmpty()) {
+                outputs.add(base);
+            } else {
+                for (String implCall : implCalls) {
+                    String implChain = base + "，" + implCall;
+                    if (controllerCalls.isEmpty()) {
+                        outputs.add(implChain);
+                    } else {
+                        for (String controllerCall : controllerCalls) {
+                            outputs.add(implChain + "，" + controllerCall);
+                        }
+                    }
+                }
+            }
+
+            return outputs;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         File rootDir = new File(args[0]);
         List<File> xmlFiles = new ArrayList<>();
         collectXmlFiles(rootDir, xmlFiles);
         //用来存储解析到的Mapper接口名和他的文件路径，方便后续去查找对应的调用
         Map<String, String> namespaceToPathMap = new HashMap<>();
-        Map<String, List<String>> namespaceToVulnerableMethodsMap = new HashMap<>();
-
+        Map<String, List<VulnerabilityDetail>> namespaceToVulnerabilitiesMap = new HashMap<>();
         for (File xmlFile : xmlFiles) {
-            scanMyBatisXML(xmlFile, namespaceToPathMap, namespaceToVulnerableMethodsMap);
+            scanMyBatisXML(xmlFile, namespaceToPathMap, namespaceToVulnerabilitiesMap);
         }
 
         List<File> javaFiles = new ArrayList<>();
@@ -49,12 +95,21 @@ public class CodeAuditMain {
         for (Map.Entry<String, String> entry : namespaceToPathMap.entrySet()) {
             String namespace = entry.getKey();
             String xmlFilePath = entry.getValue();
-            List<String> vulnerableMethods = namespaceToVulnerableMethodsMap.get(namespace);
-            findMapperInterface(namespace, xmlFilePath, javaFiles, vulnerableMethods);
+            List<VulnerabilityDetail> vulnerabilities = namespaceToVulnerabilitiesMap.get(namespace);
+            findMapperInterface(namespace, xmlFilePath, javaFiles, vulnerabilities);
         }
 
-        Map<String, List<String>> interfaceToVulnerableMethodsMap = findImplementationsAndMethodCalls(namespaceToVulnerableMethodsMap, javaFiles);
-        findRequestMappingCalls(interfaceToVulnerableMethodsMap, javaFiles);
+        Map<String, List<VulnerabilityDetail>> interfaceToVulnerabilitiesMap = findImplementationsAndMethodCalls(namespaceToVulnerabilitiesMap, javaFiles);
+        findRequestMappingCalls(interfaceToVulnerabilitiesMap, javaFiles);
+
+        for (List<VulnerabilityDetail> vulnerabilities : namespaceToVulnerabilitiesMap.values()) {
+            for (VulnerabilityDetail vulnerability : vulnerabilities) {
+                List<String> outputs = vulnerability.getFormattedOutput();
+                for (String output : outputs) {
+                    System.out.println(output);
+                }
+            }
+        }
     }
 
     public static void collectXmlFiles(File dir, List<File> xmlFiles) {
@@ -80,8 +135,9 @@ public class CodeAuditMain {
             }
         }
     }
+
     //找注入的实现
-    public static void scanMyBatisXML(File xmlFile, Map<String, String> namespaceToPathMap, Map<String, List<String>> namespaceToVulnerableMethodsMap) throws Exception {
+    public static void scanMyBatisXML(File xmlFile, Map<String, String> namespaceToPathMap, Map<String, List<VulnerabilityDetail>> namespaceToVulnerabilitiesMap) throws Exception {
         XMLInputFactory factory = XMLInputFactory.newInstance();
         XMLStreamReader reader = factory.createXMLStreamReader(new FileInputStream(xmlFile));
 
@@ -95,7 +151,7 @@ public class CodeAuditMain {
                     namespace = reader.getAttributeValue(null, "namespace");
                     if (namespace != null) {
                         namespaceToPathMap.put(namespace, xmlFile.getAbsolutePath());
-                        namespaceToVulnerableMethodsMap.put(namespace, new ArrayList<>());
+                        namespaceToVulnerabilitiesMap.put(namespace, new ArrayList<>());
                     }
                 } else if (Arrays.asList("select", "insert", "update", "delete").contains(elementName)) {
                     String id = reader.getAttributeValue(null, "id");
@@ -103,9 +159,9 @@ public class CodeAuditMain {
                     int lineNumber = reader.getLocation().getLineNumber();
 
                     if (sql.contains("${")) {
-                        System.out.printf("%s 的 %s 方法存在注入，在第 %d 行%n", xmlFile.getName(), id, lineNumber);
+                        //System.out.printf("%s 的 %s 方法存在注入，在第 %d 行%n", xmlFile.getName(), id, lineNumber);
                         if (namespace != null) {
-                            namespaceToVulnerableMethodsMap.get(namespace).add(id);
+                            namespaceToVulnerabilitiesMap.get(namespace).add(new VulnerabilityDetail(xmlFile.getName(), id, lineNumber));
                         }
                     }
                 }
@@ -126,12 +182,11 @@ public class CodeAuditMain {
         return content.toString();
     }
 
-    public static void findMapperInterface(String namespace, String xmlFilePath, List<File> javaFiles, List<String> vulnerableMethods) {
+    public static void findMapperInterface(String namespace, String xmlFilePath, List<File> javaFiles, List<VulnerabilityDetail> vulnerabilities) {
         String interfaceName = namespace.substring(namespace.lastIndexOf('.') + 1) + ".java";
 
         for (File javaFile : javaFiles) {
             if (javaFile.getName().equals(interfaceName)) {
-                boolean foundVulnerableMethod = false;
                 List<String> lines;
                 try {
                     lines = Files.readAllLines(javaFile.toPath());
@@ -140,14 +195,11 @@ public class CodeAuditMain {
                     continue;
                 }
 
-                for (String methodName : vulnerableMethods) {
+                for (VulnerabilityDetail vulnerability : vulnerabilities) {
                     for (int i = 0; i < lines.size(); i++) {
                         String line = lines.get(i).trim();
-                        if (line.startsWith("public") && line.contains(methodName + "(")) {
-                            if (!foundVulnerableMethod) {
-                                System.out.printf("%s 对应的接口文件：%s%n", Paths.get(xmlFilePath).getFileName(), javaFile.getAbsolutePath());
-                                foundVulnerableMethod = true;
-                            }
+                        if (line.startsWith("public") && line.contains(vulnerability.methodName + "(")) {
+                            break;
                         }
                     }
                 }
@@ -155,9 +207,10 @@ public class CodeAuditMain {
             }
         }
     }
+
     //查找所有实现类，并且找到调用了上面有漏洞的Mapper方法的地方
-    public static Map<String, List<String>> findImplementationsAndMethodCalls(Map<String, List<String>> namespaceToVulnerableMethodsMap, List<File> javaFiles) {
-        Map<String, List<String>> interfaceToVulnerableMethodsMap = new HashMap<>();
+    public static Map<String, List<VulnerabilityDetail>> findImplementationsAndMethodCalls(Map<String, List<VulnerabilityDetail>> namespaceToVulnerabilitiesMap, List<File> javaFiles) {
+        Map<String, List<VulnerabilityDetail>> interfaceToVulnerabilitiesMap = new HashMap<>();
 
         for (File javaFile : javaFiles) {
             try {
@@ -180,15 +233,20 @@ public class CodeAuditMain {
                                     public void visit(MethodCallExpr methodCall, Void arg) {
                                         super.visit(methodCall, arg);
                                         methodCall.getScope().ifPresent(scope -> {
-                                            namespaceToVulnerableMethodsMap.forEach((namespace, vulnerableMethods) -> {
+                                            namespaceToVulnerabilitiesMap.forEach((namespace, vulnerabilities) -> {
                                                 String mapperInterfaceName = namespace.substring(namespace.lastIndexOf('.') + 1);
-                                                if (mapperInterfaceName!=null && vulnerableMethods.contains(methodCall.getNameAsString())) {
-                                                    System.out.printf("%s 类实现了接口 %s , 调用了 %s 的 %s 方法，在 %d 行%n",
-                                                            className, String.join(", ", implementedInterfaces), mapperInterfaceName, methodCall.getNameAsString(), methodCall.getBegin().get().line);
-                                                    implementedInterfaces.forEach(interfaceName -> {
-                                                        interfaceToVulnerableMethodsMap.putIfAbsent(interfaceName, new ArrayList<>());
-                                                        interfaceToVulnerableMethodsMap.get(interfaceName).add(methodCall.getNameAsString());
-                                                    });
+                                                if (mapperInterfaceName != null && vulnerabilities.stream().anyMatch(v -> v.methodName.equals(methodCall.getNameAsString()))) {
+                                                    VulnerabilityDetail vulnerability = vulnerabilities.stream()
+                                                            .filter(v -> v.methodName.equals(methodCall.getNameAsString()))
+                                                            .findFirst().orElse(null);
+                                                    if (vulnerability != null) {
+                                                        vulnerability.addImplCall(String.format("%s 类实现了接口 %s , 调用了 %s 的 %s 方法，在第 %d 行",
+                                                                className, String.join(", ", implementedInterfaces), mapperInterfaceName, methodCall.getNameAsString(), methodCall.getBegin().get().line));
+                                                        implementedInterfaces.forEach(interfaceName -> {
+                                                            interfaceToVulnerabilitiesMap.putIfAbsent(interfaceName, new ArrayList<>());
+                                                            interfaceToVulnerabilitiesMap.get(interfaceName).add(vulnerability);
+                                                        });
+                                                    }
                                                 }
                                             });
                                         });
@@ -203,11 +261,11 @@ public class CodeAuditMain {
             }
         }
 
-        return interfaceToVulnerableMethodsMap;
+        return interfaceToVulnerabilitiesMap;
     }
 
     //参考的springboot/mvc项目，基于注解来找控制层，在控制层里找接口
-    public static void findRequestMappingCalls(Map<String, List<String>> interfaceToVulnerableMethodsMap, List<File> javaFiles) {
+    public static void findRequestMappingCalls(Map<String, List<VulnerabilityDetail>> interfaceToVulnerabilitiesMap, List<File> javaFiles) {
         for (File javaFile : javaFiles) {
             try {
                 CompilationUnit cu = StaticJavaParser.parse(javaFile);
@@ -230,11 +288,13 @@ public class CodeAuditMain {
                                         super.visit(methodCall, arg);
                                         methodCall.getScope().ifPresent(scope -> {
                                             String calledInterfaceName = scope.toString();
-                                            interfaceToVulnerableMethodsMap.forEach((interfaceName, vulnerableMethods) -> {
-                                                if (calledInterfaceName!=null && vulnerableMethods.contains(methodCall.getNameAsString())) {
-                                                    System.out.printf("%s 类的 %s 方法调用了接口 %s 的 %s 方法，在第 %s 行%n",
-                                                            controllerClassName, method.getNameAsString(), interfaceName, methodCall.getNameAsString(),methodCall.getBegin().get().line);
-                                                }
+                                            interfaceToVulnerabilitiesMap.forEach((interfaceName, vulnerabilities) -> {
+                                                vulnerabilities.forEach(vulnerability -> {
+                                                    if (calledInterfaceName != null && vulnerability.methodName.equals(methodCall.getNameAsString())) {
+                                                        vulnerability.addControllerCall(String.format("%s 类的 %s 方法调用了接口 %s 的 %s 方法，在第 %d 行%n",
+                                                                controllerClassName, method.getNameAsString(), interfaceName, methodCall.getNameAsString(), methodCall.getBegin().get().line));
+                                                    }
+                                                });
                                             });
                                         });
                                     }
