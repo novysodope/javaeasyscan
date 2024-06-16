74cb9a6de880bcf37bfa5a3b4977daebac374d3e
novy <45167857+novysodope@users.noreply.github.com>
2024-06-16 16:23:05 +0800

    Update CommandInjectScan.java

--- CommandInjectScan.java
+++ CommandInjectScan.java
@@ -106,44 +106,7 @@ public class CommandInjectScan {
             currentMethodName = methodDeclaration.getNameAsString();
             super.visit(methodDeclaration, filePath);
         }
-
-        @Override
-        public void visit(VariableDeclarator variableDeclarator, String filePath) {
-            super.visit(variableDeclarator, filePath);
-            if (variableDeclarator.getInitializer().isPresent()) {
-                Expression initializer = variableDeclarator.getInitializer().get();
-                if (initializer.isMethodCallExpr()) {
-                    MethodCallExpr methodCall = initializer.asMethodCallExpr();
-                    if (methodCall.getNameAsString().equals("getRuntime") && methodCall.getScope().isPresent() && methodCall.getScope().get().toString().equals("Runtime")) {
-                        variableDeclarations.put(variableDeclarator.getNameAsString(), "Runtime.getRuntime()");
-                    }
-                } else if (initializer.isObjectCreationExpr()) {
-                    ObjectCreationExpr objectCreationExpr = initializer.asObjectCreationExpr();
-                    if (objectCreationExpr.getType().getNameAsString().equals("ProcessBuilder")) {
-                        variableDeclarations.put(variableDeclarator.getNameAsString(), "new ProcessBuilder()");
-                    }
-                }
-            }
-        }
-
-        @Override
-        public void visit(AssignExpr assignExpr, String filePath) {
-            super.visit(assignExpr, filePath);
-            if (assignExpr.getTarget().isNameExpr() && assignExpr.getValue().isMethodCallExpr()) {
-                String variableName = assignExpr.getTarget().asNameExpr().getNameAsString();
-                MethodCallExpr methodCall = assignExpr.getValue().asMethodCallExpr();
-                if (methodCall.getNameAsString().equals("getRuntime") && methodCall.getScope().isPresent() && methodCall.getScope().get().toString().equals("Runtime")) {
-                    variableDeclarations.put(variableName, "Runtime.getRuntime()");
-                }
-            } else if (assignExpr.getTarget().isNameExpr() && assignExpr.getValue().isObjectCreationExpr()) {
-                String variableName = assignExpr.getTarget().asNameExpr().getNameAsString();
-                ObjectCreationExpr objectCreationExpr = assignExpr.getValue().asObjectCreationExpr();
-                if (objectCreationExpr.getType().getNameAsString().equals("ProcessBuilder")) {
-                    variableDeclarations.put(variableName, "new ProcessBuilder()");
-                }
-            }
-        }
-
+        
         @Override
         public void visit(MethodCallExpr methodCall, String filePath) {
             if (methodCall.getNameAsString().equals("exec")) {
@@ -161,6 +124,9 @@ public class CommandInjectScan {
                 if (isProcessBuilderStartMethod(methodCall)) {
                     int lineNumber = methodCall.getBegin().isPresent() ? methodCall.getBegin().get().line : -1;
                     StringBuilder result = new StringBuilder();
+                    methodCall.getScope().ifPresent(scope -> {
+                        logger.info("method call scope: " + scope );
+                    });
                     result.append(currentClassName).append("类存在ProcessBuilder命令执行，在").append(currentMethodName).append("方法中，第").append(lineNumber).append("行:\n");
                     result.append("<pre style=\"color:red;\">" + getLineContent(filePath, lineNumber)).append("</pre>\n");
                     findUsages(currentClassName, currentMethodName, javaFiles, javaParser, new HashSet<>(), result);
